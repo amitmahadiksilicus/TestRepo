10dbe34422535ae3aa520131247bc42f4b45ae44
Amit Mahadik <amit.mahadik@silicus.com>
2014-03-26 16:05:44 +0530

    Adding Repo

---
+++ MyCompany/MyCompany/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyCompany.MvcApplication" Language="C#" %>
---
+++ MyCompany/MyCompany/Models/MyCompany.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+namespace MyCompany.Models
+{
+    public class MyCompanyContext:DbContext
+    {
+        public MyCompanyContext(): base("db_MyCollege")
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+
+    public class Employee
+    {
+        public Employee() { }
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        public string ContactNumber { get; set; }
+       
+        public int DepartmentID { get; set; }
+        public virtual Department Department { get; set; }
+
+       
+    }
+
+    public class Department
+    {
+        public Department()
+        {
+            Employees = new List<Employee>();
+        }
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+       
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
